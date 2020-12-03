c09cea60978c1521d8780488dfcb43982207a407
muhebollah <muhebollah.diu@gamil.com>
2020-12-03 17:04:00 +0600

    add locked

--- ORDER_MANAGEMENT.API/Controllers/UserController.cs
+++ ORDER_MANAGEMENT.API/Controllers/UserController.cs
@@ -15,18 +15,18 @@ namespace ORDER_MANAGEMENT.API.Controllers
     [Authorize]
     public class UserController : ApiController
     {
-        private readonly IUnitOfWork db;
+        private readonly IUnitOfWork _db;
         public UserController(IUnitOfWork unitOfWork)
         {
-            db = unitOfWork;
+            _db = unitOfWork;
         }
         [HttpGet]
         // GET: api/TargetInfo
         [Route("api/TargetInfo")]
         public TargetInfo TargetInfo()
         {
-            var id = db.Registrations.GetRegID_ByUserName(User.Identity.Name);
-            var t_info = db.Users.API_TargetInfo(id);
+            var id = _db.Registrations.GetRegID_ByUserName(User.Identity.Name);
+            var t_info = _db.Users.API_TargetInfo(id);
             return t_info;
         }
 
@@ -34,8 +34,8 @@ namespace ORDER_MANAGEMENT.API.Controllers
         [Route("api/getUserInfo")]
         public UserNameRank getUserInfo()
         {
-            var id = db.Registrations.GetRegID_ByUserName(User.Identity.Name);
-            var U = db.Users.API_getUserInfo(id);
+            var id = _db.Registrations.GetRegID_ByUserName(User.Identity.Name);
+            var U = _db.Users.API_getUserInfo(id);
             return U;
         }
 
@@ -43,16 +43,16 @@ namespace ORDER_MANAGEMENT.API.Controllers
         [Route("api/getReportTo")]
         public UserReportTo getReportTo()
         {
-            var id = db.Registrations.GetRegID_ByUserName(User.Identity.Name);
-            return db.Users.GetReportTo(id);
+            var id = _db.Registrations.GetRegID_ByUserName(User.Identity.Name);
+            return _db.Users.GetReportTo(id);
         }
 
         // GET: api/getUserInfo
         [Route("api/getTerritory")]
         public List<DDL> getTerritory()
         {
-            var id = db.Registrations.GetRegID_ByUserName(User.Identity.Name);
-            return db.Territorys.GetUserTerritory(id);
+            var id = _db.Registrations.GetRegID_ByUserName(User.Identity.Name);
+            return _db.Territorys.GetUserTerritory(id);
         }
 
 
@@ -62,11 +62,11 @@ namespace ORDER_MANAGEMENT.API.Controllers
         {
             if (value == null) NotFound();
 
-            var RegID = db.Registrations.GetRegID_ByUserName(User.Identity.Name);
+            var RegID = _db.Registrations.GetRegID_ByUserName(User.Identity.Name);
 
-            var Reg = db.Users.UserInfoUpdate(RegID, value);
-            db.Registrations.Update(Reg);
-            db.SaveChanges();
+            var Reg = _db.Users.UserInfoUpdate(RegID, value);
+            _db.Registrations.Update(Reg);
+            _db.SaveChanges();
 
             return Ok();
         }
